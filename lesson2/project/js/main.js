7bb0c63e7e06a9996218615f9617400a02defb8d
kaelkael13 <creativeee99@outlook.com>
2022-08-11 13:49:18 +0300

    completed lesson2

--- lesson2/project/js/main.js
+++ lesson2/project/js/main.js
@@ -1,38 +1,45 @@
-class ProductList{
-    constructor(container='.products'){
+class ProductList {
+    constructor(container = '.products') {
         this.container = container;
         this.goods = [];
         this._fetchProducts();//рекомендация, чтобы метод был вызван в текущем классе
         this.render();//вывод товаров на страницу
     }
-    _fetchProducts(){
+    _fetchProducts() {
         this.goods = [
-            {id: 1, title: 'Notebook', price: 2000},
-            {id: 2, title: 'Mouse', price: 20},
-            {id: 3, title: 'Keyboard', price: 200},
-            {id: 4, title: 'Gamepad', price: 50},
+            { id: 1, title: 'Notebook', price: 2000 },
+            { id: 2, title: 'Mouse', price: 20 },
+            { id: 3, title: 'Keyboard', price: 200 },
+            { id: 4, title: 'Gamepad', price: 50 },
         ];
     }
-    
-    render(){
+
+    render() {
         const block = document.querySelector(this.container);
-        for(let product of this.goods){
-             const item = new ProductItem(product);
-             block.insertAdjacentHTML("beforeend",item.render());
-//              block.innerHTML += item.render();
+        for (let product of this.goods) {
+            const item = new ProductItem(product);
+            block.insertAdjacentHTML("beforeend", item.render());
+            //              block.innerHTML += item.render();
         }
+    }
+
+    getSum() {
+        let sum = 0;
+        this.goods.forEach(item => {
+            sum += item.price;
+        });
     }
 }
 
-class ProductItem{
-    constructor(product,img='https://via.placeholder.com/200x150'){
+class ProductItem {
+    constructor(product, img = 'https://via.placeholder.com/200x150') {
         this.title = product.title;
         this.id = product.id;
         this.price = product.price;
         this.img = img;
     }
-    render(){
-           return `<div class="product-item">
+    render() {
+        return `<div class="product-item">
                 <img src="${this.img}">
                 <h3>${this.title}</h3>
                 <p>${this.price}</p>
@@ -42,6 +49,12 @@ class ProductItem{
 }
 
 let list = new ProductList();
+
+class Cart {
+    addItem() { } // Добавить 
+    removeItem() { } // Удалить
+    changeItem() { } // Изменить
+}
 
 
 
